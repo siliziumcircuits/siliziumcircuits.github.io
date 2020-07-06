64373513d8b5b8d74a95df3792902112600acbf4
Dipak Chandran <dipak.chandran@sourcebits.com>
2020-07-06 19:33:55 +0530

    First push

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($) {
+  $(function() {
+    $(".sidenav").sidenav();
+    $(".carousel").carousel({
+      fullWidth: true,
+      indicators: true
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
